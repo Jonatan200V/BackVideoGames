375f22edf96bb63eeca1093a9799f7dd13f3bff3
Jonatan <jonatanangel88@gmail.com>
2023-01-20 20:28:37 -0300

    roolback-2

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { PASSWORD, HOSTPG, PORTPG, DBPG } = process.env;
 const sequelize = new Sequelize(
-  `postgres://postgres:${PASSWORD}@${HOSTPG}:${PORTPG}/${DBPG}`,
+  `postgresql://postgres:${PASSWORD}@${HOSTPG}:${PORTPG}/${DBPG}`,
   {
     logging: false,
     native: false,
